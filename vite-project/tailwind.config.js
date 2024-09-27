6dd9b1a5b8a329896251dd8e70747dc85eaf4f34
sailendraprasath <sailendraprasath05@gmail.com>
2024-09-27 23:47:56 +0530

    first commit

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg1: "#C23373",
+        Title: "#FFABE1",
+        Contact1: "#F5F7F8",
+        About: "#3C3D37",
+      },
+      fontFamily: {
+        welcome: "DM Serif Text serif",
+        About: "Coming Soon cursive",
+      },
+    },
+  },
+  plugins: [],
+};
